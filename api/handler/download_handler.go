f48213bf507684eb4130957f868fbf3ffdb89e2f
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2024-07-08 16:30:08 +0530

    feat: added custom download path

--- api/handler/download_handler.go
+++ api/handler/download_handler.go
@@ -31,11 +31,15 @@ func NewDownloadHandler(registry *store.ProviderRegistry, sessStore *session.Sto
 
 func (h *DownloadHandler) DownloadHandler(c *fiber.Ctx) error {
 	linksStr := c.Query("links", "")
+	destPath := c.Query("path", "")
 	if len(linksStr) == 0 {
 		return util.NewAppError(
 			http.StatusBadRequest,
 			"invalid link(s)",
 		)
+	}
+	if len(destPath) == 0 {
+		destPath = "./media"
 	}
 
 	fileIDs := util.GetFileIDs(linksStr)
@@ -63,7 +67,7 @@ func (h *DownloadHandler) DownloadHandler(c *fiber.Ctx) error {
 	}
 
 	h.downloader.FileIds = fileIDs
-	h.downloader.DestinationPath = "./media"
+	h.downloader.DestinationPath = destPath
 	h.downloader.UserID = sess.UserID
 
 	slog.Info("downloading", "GDrive fileIDs: ", fileIDs)
